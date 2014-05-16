ce3946b973cff5d09c82a09abea8ad2fe96766d0
Tony Boyles <tboyles@sosacorp.com>
2014-05-16 16:35:49 -0400

    Automated the Table's construction to fill the window's geometry and then some. Set up Excel-style Selection for single cells, still working on click-drag for selecting blocks of cells.

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,6 @@
 var noscript = document.getElementsByTagName("noscript")[0];
 noscript.parentNode.removeChild(noscript);
 
-String.prototype.titleize = function() {
-	return this.toLowerCase().replace(/(?:^|\s|-)\S/g, function(c) {
-		return c.toUpperCase();
-	});
-};
-
 currentRequest = {};
 spinner = {
 	lines : 13, // The number of lines to draw
@@ -24,23 +18,123 @@ spinner = {
 	zIndex : 2e9, // The z-index (defaults to 2000000000)
 };
 
+ab = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 yepnope({
 	load : [
 		"js/jquery-2.1.0.min.js",
 		"js/spin.min.js",
 		"js/jquery.spin.js",
 		"js/lodash.min.js",
+		"js/alertify.min.js",
 		"js/jquery-ui.min.js",
 		"js/jquery.contextMenu.min.js",
-		"js/alertify.min.js",
 		"js/jquery.tablesorter.min.js",
 		"js/jquery.tablesorter.widgets.min.js"
 	],
 	callback : {
 		"jquery-2.1.0.min.js": function(){
-			$("th").dblclick(function(){
-				var extant = $(this).text();
-				
+			addRows = function(addRows){
+				var numRows = $("tr").length;
+				var numCols = $("thead th").length;
+				for(var i = 0; i < addRows; i++){
+					newRow = $("<tr>").append("<th>" + numRows++ + "</th>");
+					for(var j = 1; j < numCols; j++){
+						newRow.append($("<td>").click(function(){
+							$("td").removeClass("selected");
+							$(this).addClass("selected");
+						}).dblclick(function(){
+							var cell = $(this);
+							cell.empty().append($("<input type='text' value='"+cell.text()+"'/>").keydown(function(evt){
+								if(evt.keyCode==13){
+									cell.empty().text(evt.target.value);
+								};
+							}));
+							cell.children().focus();
+							cell.children().focusout(function(evt){
+								cell.empty().text(evt.target.value);
+							});
+						}));
+					}
+					$("tbody").append(newRow);
+				}
+			};
+			
+			addCols = function(addCols){
+				numcols = $("thead th").length;
+				$("tr").each(function(i, row){
+					for(var j = numcols; j < numcols + addCols; j++){
+						if(i==0){
+							$(row).append("<th>" + ab[Math.floor(j/26)-1] + ab[(j-1)%26] + "</th>");
+						} else {
+							$(row).append($("<td>").click(function(){
+								$("td").removeClass("selected");
+								$(this).addClass("selected");
+							}).dblclick(function(){
+								var cell = $(this);
+								cell.empty().append($("<input type='text' value='"+cell.text()+"'/>").keydown(function(evt){
+									if(evt.keyCode==13){
+										cell.empty().text(evt.target.value);
+									};
+								}));
+								cell.children().focus();
+								cell.children().focusout(function(evt){
+									cell.empty().text(evt.target.value);
+								});
+							}));
+						}
+					}
+					$("tbody").append(newRow);
+				});
+			};
+			
+			var table = $("table");
+			if(table.height() < 2*window.innerHeight){
+				var rows = Math.ceil((2*window.innerHeight - table.height())/12);
+				addRows(rows);
+			}
+			if(table.width() < 2*window.innerWidth){
+				var cols = Math.ceil((2*window.innerWidth - table.width())/150);
+				addCols(cols);
+			}
+
+			$(window).scroll(function(evt){
+				if((window.scrollY+window.innerHeight)/window.outerHeight > .8){
+					addRows(20);
+				}
+				if((window.scrollX+window.innerWidth)/window.outerWidth > .8){
+					addCols(10);
+				}
+			});
+			
+			$(window).keydown(function(evt){
+				var cell = $("td.selected");
+				if(evt.keycode==113 && cell.length>0){
+					var extant = cell.text();
+					cell.empty().append($("<input type='text' />").val(extant).keydown(function(evt){
+						if(evt.keyCode==13){
+							cell.empty().text(evt.target.value);
+						};
+					}));
+					cell.children().focus();
+					cell.children().focusout(function(evt){
+						cell.empty().text(evt.target.value);
+					});
+				}
+			});
+
+			$("td").dblclick(function(){
+				var cell = $(this);
+				var extant = cell.text();
+				cell.empty().append($("<input type='text' />").val(extant).keydown(function(evt){
+					if(evt.keyCode==13){
+						cell.empty().text(evt.target.value);
+					};
+				}));
+				cell.children().focus();
+				cell.children().focusout(function(evt){
+					cell.empty().text(evt.target.value);
+				});
 			});
 		},
 		"jquery.spin.js" : function(){
@@ -75,6 +169,10 @@ yepnope({
 			});
 			
 			$("#github").button();
+			$("#about").button().click(function(){
+				$("#aboutcontent").dialog();
+				//alertify.alert("");
+			});
 
 			$(".datepicker").datepicker({
 				changeMonth : true,
