c046f292ca421cc1b50cd69f51d36698e48e1d67
Tony Boyles <tboyles@sosacorp.com>
2014-05-22 17:00:36 -0400

    * Rewrote the Row and Column adders.  These still need work, but it's something. * Integrated Tabular.js! * Implemented a simple uploader. Restricted to very small files (because of shitty memory handling on row/column additions) * Integrated JQ Context Menu * Moved some Javascripts around in an attempt to figure out why YepNope is failing to run some of its callbacks.

---
+++ js/Tabular.js
@@ -0,0 +1,380 @@
+window.tabular = {};
+
+tabular.table2Array = function(table) {
+  var i = 0;
+  var rowEls = table.getElementsByTagName("tr");
+  var rows = [];
+  var rawcells = rowEls[0].getElementsByTagName("th");
+  if (rawcells.length > 0) {
+    var row = [];
+    for (var j = 0; j < rawcells.length; j++) {
+      row.push(rawcells[j].innerHTML.trim());
+    }
+    rows.push(row);
+    i++;
+  }
+  for (i; i < rowEls.length; i++) {
+    var row = [];
+    rawcells = rowEls[i].getElementsByTagName("td");
+    for (var j = 0; j < rawcells.length; j++) {
+      row.push(rawcells[j].innerHTML.trim());
+    }
+    rows.push(row);
+  }
+  return rows;
+};
+
+tabular.table2Object = function(table) {
+  var rowEls = table.getElementsByTagName("tr");
+  var rawcells = rowEls[0].getElementsByTagName("th");
+  var titles = [];
+  if (rawcells.length > 0) {
+    for (var j = 0; j < rawcells.length; j++) {
+      titles.push(rawcells[j].innerHTML.trim());
+    }
+  }
+  var rows = [];
+  for (var i = 1; i < rowEls.length; i++) {
+    var row = {};
+    rawcells = rowEls[i].getElementsByTagName("td");
+    for (var j = 0; j < rawcells.length; j++) {
+      row[titles[j]] = rawcells[j].innerHTML.trim();
+    }
+    rows.push(row);
+  }
+  return rows;
+};
+
+/*
+ * table2CSV
+ * 	Given a table element, returns a CSV-formatted string.
+ *
+ * 	Optionally, you can also pass a string to be used as a delimiter between the
+ * 	cells in the output. Any instances of this string will be removed from the
+ * 	cells to allow a spreadsheet program to parse it properly, but it may cause
+ * 	some degradation in the data. For example, if the parameter is excluded, all
+ * 	commas will be removed which may cause some loss of grammatical correctness,
+ * 	large numeral readability, etc.
+ */
+tabular.table2CSV = function(table, delim) {
+  table = tabular.table2Array();
+  if (!delim) {
+    delim = ",";
+  }
+  var regex = new RegExp(delim, "g");
+  var csv = "";
+  for (var i = 0; i < table.length; i++) {
+    for (var j = 0; j < table[i].length; j++) {
+      textcells[j] = table[i][j].replace(regex, "");
+    }
+    csv += textcells.join(delim) + "\n";
+  }
+  return csv;
+};
+
+/*
+ * table2CSVURL
+ * 	Given a table element, returns a CSV-formatted, URI-encoded string.
+ */
+tabular.table2CSVURL = function(table, format) {
+  if (!format) {
+    format = "csv";
+  }
+  var url = "data:";
+  switch (format) {
+    case "csv":
+    default:
+      url += "application/csv;charset=utf-8," + encodeURI(tabular.table2CSV(table));
+      break;
+  }
+  return url;
+};
+
+/*
+ * csv2Table
+ * 	Given a CSV String, Return an HTML Table String.
+ * 	Arguments:
+ * 		csv - a CSV String
+ * 		[OPTIONAL] delim - a Delimiter String, defaults to comma (,)
+ * 		[OPTIONAL] headers - Does this CSV String include a row of headers?
+ */
+tabular.csv2Array = function(csv, delim, headers) {
+  var tableArray = tabular.csv2Array(csv);
+  if (headers !== false) {
+    headers = true;
+  }
+  var table = "<table><thead>";
+  var i = 0;
+  if (headers) {
+    table += "<tr><th>" + tableArray[i++].join("</th><th>") + "</th></tr>";
+  }
+  table += "</thead><tbody>";
+  for (i; i < tableArray.length; i++) {
+    table += "<tr><td>" + tableArray[i].join("</td><td>") + "</td></tr>";
+  }
+  return table += "</tbody><tfoot></tfoot></table>";
+};
+
+/*
+ * csv2Table
+ * 	Given a CSV String, Return an HTML Table String.
+ * 	Arguments:
+ * 		csv - a CSV String
+ * 		[OPTIONAL] delim - a Delimiter String, defaults to comma (,)
+ * 		[OPTIONAL] headers - Does this CSV String include a row of headers?
+ */
+tabular.csv2Table = function(csv, delim, headers) {
+  var tableArray = tabular.csv2Array(csv);
+  if (headers !== false) {
+    headers = true;
+  }
+  var table = "<table><thead>";
+  var i = 0;
+  if (headers) {
+    table += "<tr><th>" + tableArray[i++].join("</th><th>") + "</th></tr>";
+  }
+  table += "</thead><tbody>";
+  for (i; i < tableArray.length; i++) {
+    table += "<tr><td>" + tableArray[i].join("</td><td>") + "</td></tr>";
+  }
+  return table += "</tbody><tfoot></tfoot></table>";
+};
+
+tabular.csv2Object = function(csv) {
+
+};
+
+tabular.parser = {};
+tabular.RELAXED = true;
+tabular.IGNORE_RECORD_LENGTH = false;
+tabular.IGNORE_QUOTES = false;
+tabular.LINE_FEED_OK = true;
+tabular.CARRIAGE_RETURN_OK = true;
+tabular.DETECT_TYPES = true;
+tabular.IGNORE_QUOTE_WHITESPACE = true;
+tabular.DEBUG = false;
+tabular.ERROR_EOF = "UNEXPECTED_END_OF_FILE";
+tabular.ERROR_CHAR = "UNEXPECTED_CHARACTER";
+tabular.ERROR_EOL = "UNEXPECTED_END_OF_RECORD";
+tabular.WARN_SPACE = "UNEXPECTED_WHITESPACE";
+
+var QUOTE = "\"", CR = "\r", LF = "\n", COMMA = ",", SPACE = " ", TAB = "\t";
+
+// states
+var PRE_TOKEN = 0, MID_TOKEN = 1, POST_TOKEN = 2, POST_RECORD = 4;
+
+/**
+ * @name tabular.parse
+ * @function
+ * @description rfc4180 standard csv parse
+ * with options for strictness and data type conversion
+ * By default, will automatically type-cast numeric an boolean values.
+ * @param {String} str A csv string
+ * @return {Array} An array records, each of which is an array of scalar values.
+ * @example
+ * // simple
+ * var rows = tabular.parse("one,two,three\nfour,five,six")
+ * // rows equals [["one","two","three"],["four","five","six"]]
+ * @see http://www.ietf.org/rfc/rfc4180.txt
+ */
+tabular.parse = function(str) {
+  var result = tabular.result = [];
+  tabular.offset = 0;
+  tabular.str = str;
+  tabular.record_begin();
+
+  tabular.debug("parse()", str);
+
+  var c;
+  while (1) {
+    // pull char
+    c = str[tabular.offset++];
+    tabular.debug("c", c);
+
+    // detect eof
+    if (c == null) {
+      if (tabular.escaped)
+        tabular.error(tabular.ERROR_EOF);
+
+      if (tabular.record) {
+        tabular.token_end();
+        tabular.record_end();
+      }
+
+      tabular.debug("...bail", c, tabular.state, tabular.record);
+      tabular.reset();
+      break;
+    }
+
+    if (tabular.record == null) {
+      // if relaxed mode, ignore blank lines
+      if (tabular.RELAXED && (c == LF || c == CR && str[tabular.offset + 1] == LF)) {
+        continue;
+      }
+      tabular.record_begin();
+    }
+
+    // pre-token: look for start of escape sequence
+    if (tabular.state == PRE_TOKEN) {
+
+      if ((c === SPACE || c === TAB) && tabular.next_nonspace() == QUOTE) {
+        if (tabular.RELAXED || tabular.IGNORE_QUOTE_WHITESPACE) {
+          continue;
+        } else {
+          // not technically an error, but ambiguous and hard to debug otherwise
+          tabular.warn(tabular.WARN_SPACE);
+        }
+      }
+
+      if (c == QUOTE && !tabular.IGNORE_QUOTES) {
+        tabular.debug("...escaped start", c);
+        tabular.escaped = true;
+        tabular.state = MID_TOKEN;
+        continue;
+      }
+      tabular.state = MID_TOKEN;
+    }
+
+    // mid-token and escaped, look for sequences and end quote
+    if (tabular.state == MID_TOKEN && tabular.escaped) {
+      if (c == QUOTE) {
+        if (str[tabular.offset] == QUOTE) {
+          tabular.debug("...escaped quote", c);
+          tabular.token += QUOTE;
+          tabular.offset++;
+        } else {
+          tabular.debug("...escaped end", c);
+          tabular.escaped = false;
+          tabular.state = POST_TOKEN;
+        }
+      } else {
+        tabular.token += c;
+        tabular.debug("...escaped add", c, tabular.token);
+      }
+      continue;
+    }
+
+    // fall-through: mid-token or post-token, not escaped
+    if (c == CR) {
+      if (str[tabular.offset] == LF)
+        tabular.offset++;
+      else if (!tabular.CARRIAGE_RETURN_OK)
+        tabular.error(tabular.ERROR_CHAR);
+      tabular.token_end();
+      tabular.record_end();
+    } else if (c == LF) {
+      if (!(tabular.LINE_FEED_OK || tabular.RELAXED))
+        tabular.error(tabular.ERROR_CHAR);
+      tabular.token_end();
+      tabular.record_end();
+    } else if (c == COMMA) {
+      tabular.token_end();
+    } else if (tabular.state == MID_TOKEN) {
+      tabular.token += c;
+      tabular.debug("...add", c, tabular.token);
+    } else if (c === SPACE || c === TAB) {
+      if (!tabular.IGNORE_QUOTE_WHITESPACE)
+        tabular.error(tabular.WARN_SPACE);
+    } else if (!tabular.RELAXED) {
+      tabular.error(tabular.ERROR_CHAR);
+    }
+  }
+  return result;
+};
+
+tabular.reset = function() {
+  tabular.state = null;
+  tabular.token = null;
+  tabular.escaped = null;
+  tabular.record = null;
+  tabular.offset = null;
+  tabular.result = null;
+  tabular.str = null;
+};
+
+tabular.next_nonspace = function() {
+  var i = tabular.offset;
+  var c;
+  while (i < tabular.str.length) {
+    c = tabular.str[i++];
+    if (!(c == SPACE || c === TAB )) {
+      return c;
+    }
+  }
+  return null;
+};
+
+tabular.record_begin = function() {
+  tabular.escaped = false;
+  tabular.record = [];
+  tabular.token_begin();
+  tabular.debug("record_begin");
+};
+
+tabular.record_end = function() {
+  tabular.state = POST_RECORD;
+  if (!(tabular.IGNORE_RECORD_LENGTH || tabular.RELAXED) && tabular.result.length > 0 && tabular.record.length != tabular.result[0].length) {
+    tabular.error(tabular.ERROR_EOL);
+  }
+  tabular.result.push(tabular.record);
+  tabular.debug("record end", tabular.record);
+  tabular.record = null;
+};
+
+tabular.resolve_type = function(token) {
+  if (token.match(/^\d+(\.\d+)?$/)) {
+    token = parseFloat(token);
+  } else if (token.match(/^true|false$/i)) {
+    token = Boolean(token.match(/true/i));
+  } else if (token === "undefined") {
+    token = undefined;
+  } else if (token === "null") {
+    token = null;
+  }
+  return token;
+};
+
+tabular.token_begin = function() {
+  tabular.state = PRE_TOKEN;
+  tabular.token = "";
+};
+
+tabular.token_end = function() {
+  if (tabular.DETECT_TYPES) {
+    tabular.token = tabular.resolve_type(tabular.token);
+  }
+  tabular.record.push(tabular.token);
+  tabular.debug("token end", tabular.token);
+  tabular.token_begin();
+};
+
+tabular.debug = function() {
+  if (tabular.DEBUG)
+    console.log(arguments);
+};
+
+tabular.dump = function(msg) {
+  return [msg, "at char", tabular.offset, ":", tabular.str.substr(tabular.offset - 50, 50).replace(/\r/mg, "\\r").replace(/\n/mg, "\\n").replace(/\t/mg, "\\t")].join(" ");
+};
+
+tabular.error = function(err) {
+  var msg = tabular.dump(err);
+  tabular.reset();
+  throw msg;
+};
+
+tabular.warn = function(err) {
+  var msg = tabular.dump(err);
+  try {
+    console.warn(msg);
+    return;
+  } catch (e) {
+  }
+  try {
+    console.log(msg);
+  } catch (e) {
+  }
+};
+
+tabular.csv2Array = tabular.parse;
+tabular.parseCSV = tabular.parse;
--- js/ui.js
+++ js/ui.js
@@ -13,22 +13,24 @@ var app = {
 yepnope({
 	load: [
 		"js/spin.min.js",
-		"js/jquery-2.1.0.min.js",
-		"js/lodash.min.js",
-		"js/jquery-ui.min.js"
+		"js/jquery.min.js",
+		"js/jqueryui.min.js",
+		"js/jquery.contextMenu.min.js",
+		"js/Tabular.js",
+		"js/lodash.min.js"
 	],
-	callback : {
+	callback: {
 		"spin.min.js": function(){
 			app.spinner = new Spinner({
 				length : 100,
 				radius : 50
 			}).spin(document.getElementsByTagName("body")[0]);
 		},
-		"jquery-2.1.0.min.js": function(){
-			app.newCell = function(){
-				return $("<td>").mousedown(function(evt){
+		"jquery.min.js": function(){
+			app.newCell = function(val){
+				val = (typeof val === "undefined") ? "" : val;
+				var cell = $("<td>"+val+"</td>").mousedown(function(evt){
 					evt.preventDefault();
-					$(".selected").removeClass("selected");
 					app.dragCell = $(this);
 					$("tbody tr td, th").mousemove(function(){
 						$(".selected").removeClass("selected");
@@ -40,6 +42,7 @@ yepnope({
 							$(row).children().slice(Math.min(a,b),Math.max(a,b)+1).addClass("selected");
 						});
 					});
+					$(".selected").removeClass("selected");
 				}).mouseup(function(){
 					$("tbody tr td, th").unbind("mousemove");
 					$(this).addClass("selected");
@@ -48,72 +51,105 @@ yepnope({
 					cell.focusout(function(){
 						cell.attr("contenteditable", "false");
 					}).focus();
-				}).clone(true);
+				});
+				return cell;
+			};
+
+			app.addRows = function(rows){
+				var tbody = $("tbody");
+				var cols = $("thead th").length;
+				var extantRows = $("tr").length;
+				for(var i = 0; i < rows; i++){
+					var row = $("<tr>").append($("<th>" + (extantRows+i) + "</th>").click(function(){
+						$(".selected").removeClass("selected");
+						$(this).addClass("selected").siblings().addClass("selected");
+					}));
+					for(var j = 0; j < cols; j++){
+						row.append(app.newCell());
+					}
+					tbody.append(row);
+				}
 			};
 
 			app.addRow = function(){
-				var numRows = $("tr").length;
-				var numCols = $("thead th").length;
-				var newRow = $("<tr>").append($("<th>" + numRows++ + "</th>").click(function(){
-					$(".selected").removeClass("selected");
-					$(this).addClass("selected").siblings().addClass("selected");
-				}));
-				for(var j = 1; j < numCols; j++){
-					newRow.append(app.newCell());
-				}
-				$("tbody").append(newRow);
+				app.addRows(1);
 			};
 
-			app.addCol = function(){
+			app.addCols = function(cols){
+				cols = (typeof cols === "undefined") ? 1 : cols;
 				var j = $("thead th").length;
 				$("tr").each(function(i, row){
-					if(i==0){
-						if(j <= 702){
-							$(row).append("<th>" + app.ab[Math.ceil(j/26)-2] + app.ab[(j-1)%26] + "</th>");
+					for(k = 0; k < cols; k++){
+						if(i==0){
+							if(j+k <= 26){
+								$(row).append("<th contextmenu='headermenu'>" + app.ab[j+k-1] + "</th>");
+							} else if(j+cols <= 702){
+								$(row).append("<th contextmenu='headermenu'>" + app.ab[Math.ceil((j+k)/26)-2] + app.ab[(j+k-1)%26] + "</th>");
+							}
 						} else {
-							$(row).append("<th>" + app.ab[Math.ceil(j/702)-2] + app.ab[Math.ceil(j%702/26)-1] + app.ab[(j-1)%26] + "</th>");
+							$(row).append(app.newCell());
 						}
-					} else {
-						$(row).append(app.newCell());
 					}
+				});
+			};
+			
+			app.addCol = function(){
+				app.addCols(1);
+			};
+
+			app.emptyTable = function(){
+				$("thead").html("<tr><th></th></tr>");
+				$("tbody").empty();
+			};
+			
+			app.setTable = function(){
+				app.addCols(50);
+				app.addRows(100);
+			};
+			
+			app.resetTable = function(){
+				$("table").fadeOut(400, function(){
+					app.emptyTable();
+					app.setTable();
+					$(this).fadeIn();
 				});
 			};
 
 			$(window).scroll(function(){
 				var doc = $(document);
-				if((window.innerHeight+window.pageYOffset)/doc.height() > .5){
-					app.addRow();
+				if((window.innerHeight+window.pageYOffset)/doc.height() > .9){
+					app.addRows(5);
 				}
-				if((window.innerWidth+window.pageXOffset)/doc.width() > .6){
+				if((window.innerWidth+window.pageXOffset)/doc.width() > .9){
 					app.addCol();
 				}
 			}).keydown(function(evt){
 				var cell = $(".selected:first");
 				switch(evt.keyCode){
 					case 34: //PageDown
-						for(var i=0; i <= 30; i++){ app.addRow(); }
+						app.addRows(31);
 					break;
 					case 37: //Left
 						evt.preventDefault();
 						$(".selected").removeClass("selected");
-						cell.prev().addClass("selected");
+						cell.prev().addClass("selected").focus();
 					break;
 					case 38: //Up
 						evt.preventDefault();
 						$(".selected").removeClass("selected");
 						var sib = cell.prevAll().length;
-						$(cell.parent().prev().children()[sib]).addClass("selected");
+						$(cell.parent().prev().children()[sib]).addClass("selected").focus();
 					break;
 					case 39: //Right
 						evt.preventDefault();
 						$(".selected").removeClass("selected");
-						cell.next().addClass("selected");
+						cell.next().addClass("selected").focus();
 					break;
 					case 40: //Down
 						evt.preventDefault();
 						$(".selected").removeClass("selected");
 						var sib = cell.prevAll().length;
-						$(cell.parent().next().children()[sib]).addClass("selected");						
+						$(cell.parent().next().children()[sib]).addClass("selected").focus();						
 					case 113: //F2
 						if(cell.length>0){
 							if(cell.attr("contenteditable")!="false"){
@@ -125,62 +161,105 @@ yepnope({
 					break;
 				}
 			});
-			for(var i = 0; i < 100; i++){ app.addRow(); }
+		},
+		"jqueryui.min.js": function(){
+			$("#new").button().click(function(){
+				$("#newcontent").dialog({
+					buttons: {
+						"Yes": function(){
+							app.spinner.spin($("body")[0]);
+							$(this).dialog("close");
+							$("table").fadeOut(400, function(){
+								app.emptyTable();
+								app.addRows(100);
+								app.addCols(50);
+								$(".spinner").fadeOut(400, function(){
+									app.spinner.stop();
+									$("table").fadeIn();									
+								});
+							});
+						},
+						Cancel: function(){
+							$(this).dialog("close");
+						}
+					}
+				});
+			});
+			$("#github").button();
+			$("#about").button().click(function(){
+				$("#aboutcontent").dialog();
+			});
+		},
+		"jquery.contextMenu.min.js": function(){
+			$("thead tr").contextMenu({
+				selector: 'th',
+				callback: function(key, options) {
+               		var m = "clicked: " + key + " on " + $(this).text();
+       	        	window.console && console.log(m) || alert(m); 
+    	        },
+	            items: $.contextMenu.fromMenu($('#headermenu'))
+			});
+		},
+		"Tabular.js": function(){
+			$("#uploadfile").change(function(){
+				var reader = new FileReader();
+				reader.onloadend = function(){
+					app.csv = reader.result;
+					app.data = tabular.parse(app.csv);
+				};
+				reader.readAsText($(this).get(0).files[0]);
+			});
+			$("#upload").button().click(function(){
+				$("#uploadcontent").dialog({
+					buttons: {
+						"Let's edit this puppy!": function(){
+							$("#filename").text($("#uploadfile").get(0).files[0].name);
+							app.spinner.spin($("body")[0]);
+							$("table").fadeOut(400, function(){
+								app.addCols(app.data[0].length - $("thead th").length);
+								var rows = $("tbody tr");
+								app.addRows(app.data.length - rows.length);
+								for(var y = 0; y < app.data.length; y++){
+									var row = $(rows.get(y)).children();
+									for(var x = 0; x < app.data[y].length; x++){
+										$(row.get(x)).text(app.data[y][x]);
+									}
+								}
+								$(this).fadeIn(400, function(){
+									app.spinner.stop();
+								});
+							});
+							$(this).dialog("close");
+						},
+						Cancel: function(){
+							$(this).dialog("close");
+						}
+					}
+				});
+			});
+			$("#download").button().mousedown(function() {
+				var table = $("table");
+				var row = [];
+				table.find("th").each(function(ind, el) {
+					row.push($(el).text());
+				});
+				var csv = "";
+				table.find("tbody > tr").each(function(ind, el) {
+					row = [];
+					$(el).find("td").each(function(i, cell) {
+						row.push($(cell).text());
+					});
+					csv += row.join(",") + "\n";
+				});
+				$(this).attr({
+					"href": "data:text/csv;charset=utf8," + encodeURI(csv),
+					"download": $("#filename").text()
+				});
+			});			
 		}
 	},
 	complete : function() {
-		$("#new").button().click(function(){
-			$("#newcontent").dialog({
-				buttons: {
-					"Yes": function(){
-						$("table").fadeOut(400, function(){
-							$(".selected").removeClass("selected");
-							$("td").text("");
-							$("table").fadeIn();
-						});
-						$(this).dialog("close");
-					},
-					Cancel: function(){
-						$(this).dialog("close");
-					}
-				}
-			});
-		});
-		$("#upload").button().click(function(){
-			$("#uploadcontent").dialog({
-				buttons: {
-					"Let's edit this puppy!": function(){
-						$(this).dialog("close");
-					},
-					Cancel: function(){
-						$(this).dialog("close");
-					}
-				}
-			});
-		});
-		$("#download").button().mousedown(function() {
-			var table = $("table");
-			var row = [];
-			table.find("th").each(function(ind, el) {
-				row.push($(el).text());
-			});
-			var csv = "";
-			table.find("tbody > tr").each(function(ind, el) {
-				row = [];
-				$(el).find("td").each(function(i, cell) {
-					row.push($(cell).text());
-				});
-				csv += row.join(",") + "\n";
-			});
-			$(this).attr({
-				"href": "data:text/csv;charset=utf8," + encodeURI(csv),
-				"download": $("#filename").text()
-			});
-		});
-		$("#github").button();
-		$("#about").button().click(function(){
-			$("#aboutcontent").dialog();
-		});
+		app.setTable();
 		$(".spinner").fadeOut(400, function() {
 			app.spinner.stop();
 			$("#content").fadeIn();
